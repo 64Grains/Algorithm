6fd6c9304e5a3923ab8ea9733b133c6dbff24d7a
dquan2007 <dquan2007@163.com>
2020-02-03 12:07:30 +0800

    When quasi-uniform scatter nurbs spline, recursion may cause stack overflow, use loop instead.

--- LibraryItems/NeatSpline/NeatSpline/NurbsDeflection.h
+++ LibraryItems/NeatSpline/NeatSpline/NurbsDeflection.h
@@ -13,6 +13,8 @@
 #ifndef __NURBS_DEFLECTION_H_20200130__
 #define __NURBS_DEFLECTION_H_20200130__
 
+#include <list>
+
 /**
  * This file defines the deflection for Non-uniform rational B-spline curve.
  */
@@ -39,18 +41,11 @@ namespace neat
         // Get scatter node with given knot.
         void GetScatterNode(double nKnot_, ScatterNode<TYPE>& ScatterNode_);
 
-        // Perform scatter nurbs node between the given scope.
-        void PerformNurbs(double nDeflection_, double nStartKnot_, double nEndKnot_);
-
-        // Quasi uniform scatter nurbs node recursively.
-        void QuasiFleche(double nDeflection2_, const ScatterNode<TYPE>& StartNode_, const ScatterNode<TYPE>& EndNode_,
-            int nPointMin_, double nEps_);
+        // Quasi uniform scatter nurbs node between the given scope.
+        void QuasiFlecheNurbs(double nDeflection_, double nStartKnot_, double nEndKnot_);
 
     private:
         CNurbsParser<TYPE> m_NurbsParser;
-        CFastVector<double> m_vecValidKnots;
-        ScatterNode<TYPE> m_StartScatterNode;
-        ScatterNode<TYPE> m_EndScatterNode;
         CFastVector<ScatterNode<TYPE>>* m_pvecScatterNodes;
 
         // temporary variable reutilization
@@ -88,19 +83,19 @@ namespace neat
         m_pvecScatterNodes->push_back(ScatterNode<TYPE>());
 
         // collect the no repeat knots
-        m_vecValidKnots.clear();
-        m_vecValidKnots.push_back(pNurbsNode_->vecKnots[pNurbsNode_->nDegree]);
+        CFastVector<double> _vecValidKnots;
+        _vecValidKnots.push_back(pNurbsNode_->vecKnots[pNurbsNode_->nDegree]);
         for (int i = pNurbsNode_->nDegree, _nSize = static_cast<int>(pNurbsNode_->vecKnots.size()) - pNurbsNode_->nDegree - 1; i <= _nSize; ++i) {
-            if (!DOUBLE_EQU(pNurbsNode_->vecKnots[i], m_vecValidKnots.back())) {
-                m_vecValidKnots.push_back(pNurbsNode_->vecKnots[i]);
+            if (!DOUBLE_EQU(pNurbsNode_->vecKnots[i], _vecValidKnots.back())) {
+                _vecValidKnots.push_back(pNurbsNode_->vecKnots[i]);
             }
         }
 
         // select the middle knot between two no repeat knots
-        for (int i = 1, _nSize = static_cast<int>(m_vecValidKnots.size()); i < _nSize; ++i) {
-            double _nMiddleKnot = 0.5 * (m_vecValidKnots[i - 1] + m_vecValidKnots[i]);
-            PerformNurbs(nDeflection_, m_vecValidKnots[i - 1], _nMiddleKnot);
-            PerformNurbs(nDeflection_, _nMiddleKnot, m_vecValidKnots[i]);
+        for (int i = 1, _nSize = static_cast<int>(_vecValidKnots.size()); i < _nSize; ++i) {
+            double _nMiddleKnot = 0.5 * (_vecValidKnots[i - 1] + _vecValidKnots[i]);
+            QuasiFlecheNurbs(nDeflection_, _vecValidKnots[i - 1], _nMiddleKnot);
+            QuasiFlecheNurbs(nDeflection_, _nMiddleKnot, _vecValidKnots[i]);
         }
     }
 
@@ -117,65 +112,80 @@ namespace neat
     }
 
     template<typename TYPE>
-    void CNurbsQuasiUniformDeflection<TYPE>::PerformNurbs(double nDeflection_, double nStartKnot_, double nEndKnot_)
+    void CNurbsQuasiUniformDeflection<TYPE>::QuasiFlecheNurbs(double nDeflection_, double nStartKnot_, double nEndKnot_)
     {
-        int _nPointMin = 2;
-
-        GetScatterNode(nStartKnot_, m_StartScatterNode);
-        GetScatterNode(nEndKnot_, m_EndScatterNode);
+        ScatterNode<TYPE> _StartScatterNode, _EndScatterNode;
+        GetScatterNode(nStartKnot_, _StartScatterNode);
+        GetScatterNode(nEndKnot_, _EndScatterNode);
 
         m_pvecScatterNodes->pop_back();
-        m_pvecScatterNodes->push_back(m_StartScatterNode);
+        m_pvecScatterNodes->push_back(_StartScatterNode);
 
-        const double _nTolerance2 = Precision::SquareRealTolerance();
-        QuasiFleche(nDeflection_ * nDeflection_, m_StartScatterNode, m_EndScatterNode, _nPointMin, _nTolerance2);
-    }
+        const double _nSquareTolerance = Precision::SquareRealTolerance();
+        const double _nSquareDeflection = nDeflection_ * nDeflection_;
+        const int _nPointMin = 2;
 
-    template<typename TYPE>
-    void CNurbsQuasiUniformDeflection<TYPE>::QuasiFleche(double nDeflection2_, const ScatterNode<TYPE>& StartNode_, const ScatterNode<TYPE>& EndNode_,
-        int nPointMin_, double nEps_)
-    {
-        int _nPointSize = static_cast<int>(m_pvecScatterNodes->size());
-        double _nDeltaKnot = EndNode_.nKnot - StartNode_.nKnot;
-        ScatterNode<TYPE> _NextNode = EndNode_;
-        if (nPointMin_ > 2) {
-            _nDeltaKnot /= (nPointMin_ - 1);
-            GetScatterNode(StartNode_.nKnot + _nDeltaKnot, _NextNode);
-        }
+        // Unprocessed nodes
+        struct UndoNodes
+        {
+            ScatterNode<TYPE> node;
+            int nPointMin = 0;
+        };
 
-        double _nNorme = (_NextNode.ptPoint - StartNode_.ptPoint).GetSquareLength();
-        double _nTheFleche = 0.;
-        bool _bFlecheOk = false;
-        if (_nNorme > nEps_) {
-            double _nNorme1 = StartNode_.ptDeriv1.GetSquareLength();
-            double _nNorme2 = _NextNode.ptDeriv1.GetSquareLength();
-            if (_nNorme1 > nEps_&& _nNorme2 > nEps_) {
-                m_nxTempPoints[0] = StartNode_.ptDeriv1 / StartNode_.ptDeriv1.GetLength();
-                m_nxTempPoints[1] = _NextNode.ptDeriv1 / _NextNode.ptDeriv1.GetLength();
-                double _nNormeDiff = (m_nxTempPoints[0] - m_nxTempPoints[1]).GetSquareLength();
-                if (_nNormeDiff > nEps_) {
-                    _nTheFleche = _nNormeDiff * _nNorme / 64.0;
-                    _bFlecheOk = true;
+        std::list<UndoNodes> _listUndoNodes;
+        _listUndoNodes.emplace_front(UndoNodes{ _EndScatterNode, _nPointMin });
+
+        // recursion could stack overflow, loop instead
+        ScatterNode<TYPE> _NextKnot;
+        while (!_listUndoNodes.empty()) {
+            ScatterNode<TYPE>& _StartKnot = m_pvecScatterNodes->back();
+            UndoNodes& _UndoEndKnot = _listUndoNodes.front();
+
+            int _nPointSize = static_cast<int>(m_pvecScatterNodes->size());
+            double _nDeltaKnot = _UndoEndKnot.node.nKnot - _StartKnot.nKnot;
+            _NextKnot = _UndoEndKnot.node;
+            if (_UndoEndKnot.nPointMin > 2) {
+                _nDeltaKnot /= (_UndoEndKnot.nPointMin - 1);
+                GetScatterNode(_StartKnot.nKnot + _nDeltaKnot, _NextKnot);
+            }
+
+            double _nNorme = (_NextKnot.ptPoint - _StartKnot.ptPoint).GetSquareLength();
+            double _nTheFleche = 0.0;
+            bool _bFlecheOK = false;
+            if (_nNorme > _nSquareTolerance) {
+                double _nNorme1 = _StartKnot.ptDeriv1.GetSquareLength();
+                double _nNorme2 = _NextKnot.ptDeriv1.GetSquareLength();
+                if (_nNorme1 > _nSquareTolerance && _nNorme2 > _nSquareTolerance) {
+                    m_nxTempPoints[0] = _StartKnot.ptDeriv1 / _StartKnot.ptDeriv1.GetLength();
+                    m_nxTempPoints[1] = _NextKnot.ptDeriv1 / _NextKnot.ptDeriv1.GetLength();
+                    double _nNormeDiff = (m_nxTempPoints[0] - m_nxTempPoints[1]).GetSquareLength();
+                    if (_nNormeDiff > _nSquareTolerance) {
+                        _nTheFleche = _nNormeDiff * _nNorme / 64.0;
+                        _bFlecheOK = true;
+                    }
                 }
             }
-        }
 
-        if (!_bFlecheOk) {
-            m_nxTempPoints[2] = (StartNode_.ptPoint + _NextNode.ptPoint) * 0.5;
-            m_NurbsParser.GetNurbsPoint(StartNode_.nKnot + 0.5 * _nDeltaKnot, m_nxTempPoints[3]);
-            _nTheFleche = (m_nxTempPoints[2] - m_nxTempPoints[3]).GetSquareLength();
-        }
+            if (!_bFlecheOK) {
+                m_nxTempPoints[2] = (_StartKnot.ptPoint + _NextKnot.ptPoint) * 0.5;
+                m_NurbsParser.GetNurbsPoint(_StartKnot.nKnot + 0.5 * _nDeltaKnot, m_nxTempPoints[3]);
+                _nTheFleche = (m_nxTempPoints[2] - m_nxTempPoints[3]).GetSquareLength();
+            }
 
-        if (_nTheFleche < nDeflection2_) {
-            m_pvecScatterNodes->push_back(_NextNode);
-        }
-        else {
-            QuasiFleche(nDeflection2_, StartNode_, _NextNode, 3, nEps_);
-        }
+            if (_nTheFleche < _nSquareDeflection) {
+                m_pvecScatterNodes->push_back(_NextKnot);
+            }
+            else {
+                _listUndoNodes.emplace_front(UndoNodes{ _NextKnot, 3 });
+                continue;
+            }
 
-        if (nPointMin_ > 2) {
-            int _nPointMin = nPointMin_ - (static_cast<int>(m_pvecScatterNodes->size()) - _nPointSize);
-            QuasiFleche(nDeflection2_, _NextNode, EndNode_, _nPointMin, nEps_);
+            if (_UndoEndKnot.nPointMin > 2) {
+                _UndoEndKnot.nPointMin += _nPointSize - static_cast<int>(m_pvecScatterNodes->size());
+            }
+            else {
+                _listUndoNodes.pop_front();
+            }
         }
     }
 
